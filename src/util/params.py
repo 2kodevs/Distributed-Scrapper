300cee7801895e1dd2ab96a9d30634aa7f24d3a3
AntiD2ta <mtpotro41@gmail.com>
2019-11-03 01:55:14 -0500

    [util] refs #1 - Add util folder and params.py for useful fixed values.

---
+++ src/util/params.py
@@ -0,0 +1,9 @@
+urls = [
+    "https://rpyc.readthedocs.io/en/latest/tutorial/tut3.html",
+    "https://gobyexample.com/",
+    "http://www.cubadebate.cu/",
+    "https://www.wikipedia.org/"
+]
+
+format  = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+datefmt ='%Y-%m-%d %H:%M:%S'
