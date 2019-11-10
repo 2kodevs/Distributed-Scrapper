dc4547dfeb1b8364e39a18a7f7df38a3d55341a3
AntiD2ta <mtpotro41@gmail.com>
2019-11-09 19:46:28 -0500

    [util] - Add util file with helper methods for get the local ip address.

---
+++ util/utils.py
@@ -0,0 +1,13 @@
+import socket
+
+def getIp():
+    """
+    Return local ip address(it must be connected to internet).
+    """
+    return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+
+def getIpOffline():
+    """
+    Return local ip address(works on LAN without internet).
+    """
+    return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
