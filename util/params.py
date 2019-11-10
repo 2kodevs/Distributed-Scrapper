c47fab57f2a43d1aeb05f71bd0f686ac5b17ef01
AntiD2ta <mtpotro41@gmail.com>
2019-11-09 19:47:25 -0500

    [util] - Add initial fixed seeds.

--- util/params.py
+++ util/params.py
@@ -1,9 +1,13 @@
 urls = [
+    "http://www.cubadebate.cu/",
     "https://rpyc.readthedocs.io/en/latest/tutorial/tut3.html",
     "https://gobyexample.com/",
-    "http://www.cubadebate.cu/",
     "https://www.wikipedia.org/"
 ]
 
 format  = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 datefmt ='%Y-%m-%d %H:%M:%S'
+
+localhost = "localhost"
+client_addr = localhost + ":" + "8101"
+seeds = [localhost + ":" + "8102"]
