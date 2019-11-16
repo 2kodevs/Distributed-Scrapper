0be88f12f5a71d923f4805aa3939c2660d8616fe
Raul Iglesias <riglesiasvera@gmail.com>
2019-11-16 18:46:59 -0500

    [scrapper] refs #10 - Pulling only when slaves are available
    Use `multiprocessing.Value` to count the number of bussy slaves

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import zmq, logging, time, os, requests, pickle
 from util.params import seeds, localhost
-from multiprocessing import Process, Lock, Queue
+from multiprocessing import Process, Lock, Queue, Value
+from ctypes import c_int
 from threading import Thread, Lock as tLock
 from util.params import format, datefmt, login
 from util.utils import parseLevel
@@ -8,7 +9,8 @@ from util.utils import parseLevel
 logging.basicConfig(format=format, datefmt=datefmt)
 log = logging.getLogger(name="Scrapper")
 
-lockQueue = Lock()
+availableSlaves = Value(c_int)
+
 lockClients = tLock()
 lockSocketPull = tLock()
 
@@ -20,6 +22,7 @@ def slave(tasks, uuid):
     socket = context.socket(zmq.REQ)
     while True:
         clientAddr, url = tasks.get() 
+        availableSlaves.value -= 1
         log.info(f"Child:{os.getpid()} of Scrapper:{uuid} downloading {url}")
         response = requests.get(url)
         log.debug(f"Child:{os.getpid()} of Scrapper:{uuid} connecting to {clientAddr}")
@@ -28,20 +31,20 @@ def slave(tasks, uuid):
         socket.send_json(("RESULT", url, response.text))
         #nothing important to receive
         socket.recv()
+        availableSlaves.value += 1
+  
         
 def addClient(clientId, addr, port, clients:dict, clientQueue, uuid):
-    lockClients.acquire()
-    try:
-        if clients[clientId] != (addr, port):
+    with lockClients:
+        try:
+            if clients[clientId] != (addr, port):
+                clients[clientId] = (addr, port)
+                clientQueue.put((clientId, (addr, port))) 
+                log.debug(f"Client(id:{clientId}, address:{addr}) added to the queue of Scrapper:{uuid}")
+        except KeyError:
             clients[clientId] = (addr, port)
-            clientQueue.put((clientId, (addr, port))) 
+            clientQueue.put((clientId, (addr, port)))  
             log.debug(f"Client(id:{clientId}, address:{addr}) added to the queue of Scrapper:{uuid}")
-    except KeyError:
-        clients[clientId] = (addr, port)
-        clientQueue.put((clientId, (addr, port)))  
-        log.debug(f"Client(id:{clientId}, address:{addr}) added to the queue of Scrapper:{uuid}")
-    finally:
-        lockClients.release()
 
 
 def discoverClients(clients:dict, clientQueue, uuid):
@@ -62,11 +65,13 @@ def discoverClients(clients:dict, clientQueue, uuid):
         log.info(f"Scrapper:{uuid} has received a new client(id:{clientId}, address:{addr[0]}:{addr[1]})")
         addClient(clientId, addr[0], addr[1], clients, clientQueue, uuid)
 
+
 def connectToClients(socket, clientQueue, uuid):
     for clientId, addr in iter(clientQueue.get, "STOP"):
         with lockSocketPull:
             socket.connect(f"tcp://{addr[0]}:{addr[1]}")
             log.info(f"Scrapper:{uuid} connected to client(id:{clientId}, address:{addr[0]}:{addr[1]})")
+
 
 def publishClients(addr, port, clients:dict, clientQueue, uuid):
     """
@@ -94,8 +99,8 @@ def publishClients(addr, port, clients:dict, clientQueue, uuid):
         addClient(clientId, clientAddr, clientPort, clients, clientQueue, uuid)
         log.info(f"Scrapper-Seed:{uuid} publishing new client(id:{clientId}, address:{clientAddr, clientPort})")
         socketPub.send_multipart([b"NEW_CLIENT", pickle.dumps((clientId, (clientAddr, clientPort)))])
-        
 
+        
 class Scrapper:
     """
     Represents a scrapper, the worker node in the Scrapper network.
@@ -132,6 +137,7 @@ class Scrapper:
 
         taskQueue = Queue()
         log.info(f"Scrapper:{self.uuid} starting child process")
+        availableSlaves.value = slaves
         for _ in range(slaves):
             p = Process(target=slave, args=(taskQueue, self.uuid))
             p.start()
@@ -140,14 +146,17 @@ class Scrapper:
         while True:
             #//HACK: We need a condition here between the process, such one that we pull another task only if a slave has finish one.
             #task: (client_addr, url)
-            task = socketPull.recv_json()
-            log.debug(f"Pulled {task[1]} in worker:{self.uuid}")
-            taskQueue.put(task)
+            if availableSlaves.value > 0:
+                task = socketPull.recv_json()
+                log.debug(f"Pulled {task[1]} in worker:{self.uuid}")
+                taskQueue.put(task)                    
+
 
 def main(args):
     log.setLevel(parseLevel(args.level))
     s = Scrapper(2, port=args.port, seed=args.seed, address=args.address)
     s.manage(2)
+
             
 if __name__ == "__main__":
     import argparse
