f618f8000584db6f45f7fe85641420cdf59d518b
Raul Iglesias <riglesiasvera@gmail.com>
2019-12-04 14:28:13 -0500

    [Makefile] -Add a rule to copy urls into urls default file

--- dispatcher.py
+++ dispatcher.py
@@ -315,8 +315,8 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--address', type=str, default='127.0.0.1', help='node address')
     parser.add_argument('-p', '--port', type=int, default=4142, help='connection port')
     parser.add_argument('-l', '--level', type=str, default='DEBUG', help='log level')
-    parser.add_argument('-d', '--depth', type=int, default=1, help='recursively downloads depth')
-    parser.add_argument('-u', '--urls', type=str, default='urls', help='file that contains the urls set')
+    parser.add_argument('-d', '--depth', type=int, default=1, help='depth of recursive downloads')
+    parser.add_argument('-u', '--urls', type=str, default='urls', help='path of file that contains the urls set')
     parser.add_argument('-s', '--seed', type=str, default=None, help='address of an existing seed node. Insert as ip_address:port_number')
 
 
