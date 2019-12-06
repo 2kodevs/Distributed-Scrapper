1018c2bbdb358dfefc93a46757242759647512c2
Raul Iglesias <riglesiasvera@gmail.com>
2019-12-05 23:43:13 -0500

    [util] - Add a function to run, wait and return the value of a process

--- util/utils.py
+++ util/utils.py
@@ -242,3 +242,16 @@ def findSeeds(seeds, peerQs, deadQs, log, timeout=1000, sleepTime=15, seedFromIn
 
         #//TODO: Change the amount of the sleep in production
         time.sleep(sleepTime)
+        
+        
+def run_process(target, args):
+    """
+    Run a process and return it's result.
+    """
+    ansQ = Queue()
+    args = args + (ansQ,)
+    process = Process(target=target, args=args)
+    process.start()
+    ans = ansQ.get()
+    process.terminate()
+    return ans
